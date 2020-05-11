bf78531b369f65af18c3bbd35f8fa0b4f38137b5
David Michael Gregg <davegregg@gmail.com>
2020-05-11 17:59:13 -0400

    Update main.js

--- 01-hangman-lite/main.js
+++ 01-hangman-lite/main.js
@@ -1,17 +1,22 @@
 /*
-  HANGMAN LITE
+  LETTER COUNT
 
-  1. Create a variable (`word`) which contains any lengthy word.
-  2. Create a second variable (`letter`) which indicates the letter
-     that the player "guessed" (optional: use an input textbox and
-     click event listener to obtain an actual player guess).
+  1. Create a variable, named "word," which contains any lengthy word.
+  2. Create a second variable, named "letter," which contains any
+      letter. This simulates the letter that the player "guessed."
   3. Write a WHILE statement which loops over the `word` string and
      counts the number of times the "guessed" letter appears.
-  4. If the letter is found in the string, display "The letter X
-     exists N times in this word!" on the page (replacing "X" with
+  4. If the letter is found in the string, display on the page: "The letter X
+     exists N times in this word!" (replacing "X" with
      the letter guessed and "N" with the letter count).
-  5. If the letter isn't found in the string, display "Nope, that
-     letter doesn't exist in my word!" on the page.
+  5. If the letter isn't found in the string, display on the page: "Nope, that
+     letter doesn't exist in my word!"
+  6. OPTIONAL: Use an input textbox and click event listener to obtain
+     an actual player guess for Point 2.
+
+  TIP: What do we mean by "display on the page"? `.innerHTML` could achieve
+       this!
+
 */
 
 // YOUR CODE HERE
