84b68a7b053fd27ac76dca1033b29a04570fcdc5
tamburkey <petem14@gmail.com>
2020-05-13 10:26:40 -0400

    update wording

--- 01-letter-count/main.js
+++ 01-letter-count/main.js
@@ -3,16 +3,16 @@
 
   1. Create a variable, named "word," which contains any lengthy word.
   2. Create a second variable, named "letter," which contains any
-      letter. This simulates the letter that the player "guessed."
-  3. Write a WHILE statement which loops over the `word` string and
-     counts the number of times the "guessed" letter appears.
+      letter. 
+  3. Write a WHILE statement which loops over the "word" string and
+     counts the number of times the "letter" appears.
   4. If the letter is found in the string, display on the page: "The letter X
      exists N times in this word!" (replacing "X" with
      the letter guessed and "N" with the letter count).
   5. If the letter isn't found in the string, display on the page: "Nope, that
      letter doesn't exist in my word!"
   6. OPTIONAL: Use an input textbox and click event listener to obtain
-     an actual player guess for Point 2.
+     a guess from a user's input for Point 2.
 
   TIP: What do we mean by "display on the page"? `.innerHTML` could achieve
        this!
